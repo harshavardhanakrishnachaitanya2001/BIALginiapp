5d8022631d9e83433e2c6761d5866bfc8dc8df00
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2022-03-15 20:52:56 +0530

    building the payment page

--- pages/Payment.js
+++ pages/Payment.js
@@ -1,5 +1,28 @@
+import styles from '../styles/payment.module.css'
 export default function Payment(){
     return <>
-        <h1>Payment</h1>
+        <h1 className={styles.mainHeading}>Payment</h1>
+        <form className={styles.paymentMode}>
+            <div>
+                <label>Card number: </label>
+                <input type="text"/>
+            </div>
+            <div>
+                <label>Name on card: </label>
+                <input type="text"/>
+            </div>
+            <div>
+                <label>Expairy date: </label>
+                <input type="text"/>
+            </div>
+            <div>
+                <label>CVV: </label>
+                <input type="password"/>
+            </div>
+            <div>
+                <label>Save card details</label>
+                <input type="checkbox" checked="true"/>
+            </div>
+        </form>
     </>
 }
