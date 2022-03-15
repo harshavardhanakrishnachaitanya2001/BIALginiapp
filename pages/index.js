6df4f4d3613e5eeb5e0ecc405dfdc7507a2e3506
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2022-03-15 15:50:20 +0530

    Successfully added tailwind, a payment page and working on the signup/signin page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
-export default function Page1(){
+import {useState} from 'react'
+export default function SignUpSignIn(){
   return <>
     <h1>BIAL</h1>
+    <form>
+      <div>
+        <label htmlFor='username'>Username: </label>
+        <input type="text" name="username"/>
+      </div>
+      <div>
+        <label htmlFor='phone'>Phone number: </label>
+        <input type="text" name="phone"/>
+      </div>
+      <div>
+        <label htmlFor='email'>Email: </label>
+        <input type="text" name="email"/>
+      </div>
+      <div>
+        <label htmlFor='address'>Address: </label>
+        <input type="text" name="address"/>
+      </div>
+      <div>
+        <label htmlFor='password'>Password: </label>
+        <input type="text" name="password"/>
+      </div>
+    </form>
     
   </>
 }
