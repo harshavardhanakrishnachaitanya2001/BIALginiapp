188da94a809349093c072286888e90799713b37c
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2022-03-15 18:20:31 +0530

    trying to style with tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,29 @@
 import {useState} from 'react'
 export default function SignUpSignIn(){
   return <>
-    <h1>BIAL</h1>
+    <h1 className='text-center'>BIAL</h1>
     <form>
       <div>
         <label htmlFor='username'>Username: </label>
-        <input type="text" name="username"/>
+        <input type="text" autoComplete='off' name="username"/>
       </div>
       <div>
         <label htmlFor='phone'>Phone number: </label>
-        <input type="text" name="phone"/>
+        <input type="text" autoComplete='off' name="phone"/>
       </div>
       <div>
         <label htmlFor='email'>Email: </label>
-        <input type="text" name="email"/>
+        <input type="text" autoComplete='off' name="email"/>
       </div>
       <div>
         <label htmlFor='address'>Address: </label>
-        <input type="text" name="address"/>
+        <input type="text" autoComplete='off' name="address"/>
       </div>
       <div>
         <label htmlFor='password'>Password: </label>
-        <input type="text" name="password"/>
+        <input type="text" autoComplete='off' name="password"/>
       </div>
+      <button type="submit">Register</button>
     </form>
     
   </>
