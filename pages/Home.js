9960e8982dd2dc3f199d508893ecdd20db3431cb
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2022-03-16 12:33:45 +0530

    Working on the structure of page 2

--- pages/Home.js
+++ pages/Home.js
@@ -1,5 +1,25 @@
+import styles from '../styles/Home.module.css'
 export default function Home(){
     return <>
-        <h1>Available flights</h1>
+        <h1 className={styles.mainHeading}>Available flights</h1>
+        <div className={styles.inputs}>
+        <label>Destination: </label>
+        <input type="text"/>
+        <label>Date of departuer: </label>
+        <input type="date"/>
+        <label>Return date: </label>
+        <input type="date"/>
+        </div>
+        <div className={styles.tableContainer}>
+            <table className={styles.availableFlights}>
+                <tr>
+                    <td>Available flights</td>
+                    <td>Timings</td>
+                    <td>Bording pt.</td>
+                    <td>Available seats</td>
+                    <td>Book?</td>
+                </tr>
+            </table>
+        </div>
     </>
 }
