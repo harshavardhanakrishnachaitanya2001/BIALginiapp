513ef01ff5115a23ef134625b9eb8cc8d0d8458b
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2022-03-16 09:58:27 +0530

    Storing sign up details in an object

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,57 @@
 import {useState} from 'react'
 import styles from '../styles/Signup.module.css'
 export default function SignUpSignIn(){
+  const [signupDetails,setSignupDetails] = useState({});
+  const [username,setUsername] = useState('');
+  const [phone,setPhone] = useState();
+  const [email,setEmail] = useState('');
+  const [address,setAddress] = useState('');
+  const [password,setPassword] = useState('');
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+  const handlePhone = (e) => {
+    setPhone(e.target.value);
+  }
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  const handleAddress = (e) => {
+    setAddress(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSignupDetails({'username':username,'phone':phone, 'email':email, 'address':address, 'password':password})
+  }
   return <>
     <h1 className={styles.mainHeading}>Bangulure International Airport Limited</h1>
     <form className={styles.signupform}>
       <div>
         <label htmlFor='username'>Username: </label>
-        <input type="text" autoComplete='off' name="username"/>
+        <input type="text" autoComplete='off' name="username" onChange = {handleUsername} value = {username}/>
       </div>
       <div>
         <label htmlFor='phone'>Phone number: </label>
-        <input type="text" autoComplete='off' name="phone"/>
+        <input type="text" autoComplete='off' name="phone" onChange = {handlePhone} value = {phone}/>
       </div>
       <div>
         <label htmlFor='email'>Email: </label>
-        <input type="text" autoComplete='off' name="email"/>
+        <input type="text" autoComplete='off' name="email" value={email} onChange={handleEmail}/>
       </div>
       <div>
         <label htmlFor='address'>Address: </label>
-        <textarea autoComplete="off" name="address"/>
+        <textarea autoComplete="off" name="address" onChange={handleAddress} value={address}/>
       </div>
       <div>
         <label htmlFor='password'>Password: </label>
-        <input type="text" autoComplete='off' name="password"/>
+        <input type="password" autoComplete='off' name="password" value = {password} onChange = {handlePassword}/>
       </div>
-      <button type="submit">Register</button>
+      <button type="submit" onClick = {handleSubmit}>Register</button>
     </form>
+    <p>{signupDetails.username} {signupDetails.phone} {signupDetails.email} {signupDetails.address} {signupDetails.password}</p>
     
   </>
 }
